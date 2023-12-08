42c4d1467bc55263dc7a1ed755d8fba79348c121
Wesley Lira <wesleeylirac12@gmail.com>
2023-12-08 10:02:11 -0300

    feat: adding routing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [
+    CommonModule, 
+    HeaderComponent, 
+    RouterLink, 
+    RouterLinkActive, 
+    RouterOutlet],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
