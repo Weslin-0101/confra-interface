43caa57d0cf1065f4b8ddec971ffe6a42e7ce3a8
Wesley Lira <wesleeylirac12@gmail.com>
2023-12-26 22:02:50 -0300

    refactor: refactoring login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     CommonModule, 
-    HeaderComponent, 
-    RouterLink, 
-    RouterLinkActive, 
-    RouterOutlet],
+    HeaderComponent,
+    SignupComponent
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
